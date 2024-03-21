317c8b361f2b3c33c126d92bda3596c80ef5709f
Ginaldo-Silva <138523497+Ginaldo-Silva@users.noreply.github.com>
2024-03-21 00:13:55 -0300

    Calog.
    Inicio da estilização e construção do catalogo!

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const burgerInput = document.getElementById('burger');
 const nav = document.getElementById('nav');
+const colectionText = document.getElementById('colectionText');
+const switchColectionBtn = document.getElementById('switch-colection-btn');
+let infantil = true;
+
+function start() {
+    switchColection();
+}
 
 function SwitchNav() {
     if(burgerInput.checked) {
@@ -15,6 +22,26 @@ function SwitchNav() {
     }
 }
 
+function switchColection() {
+    infantil = !infantil;
+    if(infantil) {
+        if(colectionText.classList.contains('text-detail')){
+            colectionText.classList.remove('text-detail');
+        }
+        colectionText.classList.add('teenText');
+        colectionText.innerHTML = 'Infantil';
+        switchColectionBtn.innerHTML = 'Adulto';
+    } else {
+        if(colectionText.classList.contains('teenText')){
+            colectionText.classList.remove('teenText');
+        }
+        colectionText.classList.add('text-detail');
+        colectionText.innerHTML = 'Adulto';
+        switchColectionBtn.innerHTML ='Infantil';
+    }
+    console.log(infantil);
+}
+
 document.addEventListener('mousedown', (event) => {
     if(burgerInput.checked) {
         if(!nav.contains(event.target) && !event.target.closest('.burger')) {
@@ -24,4 +51,5 @@ document.addEventListener('mousedown', (event) => {
     }
 });
 
+switchColectionBtn.addEventListener('click', switchColection);
 burgerInput.addEventListener('change', SwitchNav);
